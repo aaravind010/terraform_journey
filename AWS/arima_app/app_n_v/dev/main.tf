97fad0ef75dfda1b6f77c68010f08ad73339e07c
aaravind010 <31498409+aaravind010@users.noreply.github.com>
2025-01-29 04:18:50 +0000

    we_server

--- AWS/arima_app/app_n_v/dev/main.tf
+++ AWS/arima_app/app_n_v/dev/main.tf
@@ -7,8 +7,8 @@ resource "aws_vpc" "arima_vpc" {
 
 resource "aws_subnet" "arima_public_subnet_1" {
   vpc_id                  = aws_vpc.arima_vpc.id
-  cidr_block              = var.arima_public_subnet_1
-  availability_zone       = var.arima_public_subnet_az_1
+  cidr_block              = var.arima_public_subnet["pub_1"]
+  availability_zone       = var.arima_public_azs["pub_1"]
   map_public_ip_on_launch = true
   tags = {
     Name = "Public Subnet 1"
@@ -17,8 +17,8 @@ resource "aws_subnet" "arima_public_subnet_1" {
 
 resource "aws_subnet" "arima_public_subnet_2" {
   vpc_id                  = aws_vpc.arima_vpc.id
-  cidr_block              = var.arima_public_subnet_2
-  availability_zone       = var.arima_public_subnet_az_2
+  cidr_block              = var.arima_public_subnet["pub_2"]
+  availability_zone       = var.arima_public_azs["pub_2"]
   map_public_ip_on_launch = true
   tags = {
     Name = "Public Subnet 2"
@@ -48,8 +48,8 @@ resource "aws_route_table_association" "public_rt_1_association" {
 
 resource "aws_subnet" "arima_private_subnet_1" {
   vpc_id            = aws_vpc.arima_vpc.id
-  cidr_block        = var.arima_private_subnet_1
-  availability_zone = var.arima_private_subnet_az_1
+  cidr_block        = var.arima_private_subnet["pri_1"]
+  availability_zone = var.arima_private_azs["pri_1"]
   tags = {
     Name = "Private Subnet 1"
   }
@@ -57,8 +57,8 @@ resource "aws_subnet" "arima_private_subnet_1" {
 
 resource "aws_subnet" "arima_private_subnet_2" {
   vpc_id            = aws_vpc.arima_vpc.id
-  cidr_block        = var.arima_private_subnet_2
-  availability_zone = var.arima_private_subnet_az_2
+  cidr_block        = var.arima_private_subnet["pri_2"]
+  availability_zone = var.arima_private_azs["pri_2"]
   tags = {
     Name = "Private Subnet 2"
   }
@@ -87,4 +87,36 @@ resource "aws_route_table" "arima_private_rt_1" {
 resource "aws_route_table_association" "private_rt_1_association" {
   route_table_id = aws_route_table.arima_private_rt_1.id
   subnet_id      = aws_subnet.arima_private_subnet_1.id
+}
+
+resource "aws_key_pair" "terra" {
+  public_key = file("terra.pub")
+  key_name   = "terra-ssh-key"
+}
+
+resource "aws_instance" "tera_inst" {
+  instance_type = var.ec2_instance_type["pub"]
+  subnet_id     = aws_subnet.arima_public_subnet_1.id
+  ami           = var.ec2_ami_type["pub"]
+
+  connection {
+    host        = self.public_ip
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("terra")
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install php php-mysqlnd httpd -y",
+      "wget https://wordpress.org/wordpress-4.8.14.tar.gz",
+      "tar -xzf wordpress-4.8.14.tar.gz",
+      "sudo cp -r wordpress /var/www/html/",
+      "sudo chown -R apache.apache /var/www/html/",
+      "sudo systemctl start httpd",
+      "sudo systemctl enable httpd",
+      "sudo systemctl restart httpd"
+    ]
+  }
 }
--- AWS/arima_app/app_n_v/dev/vars.tf
+++ AWS/arima_app/app_n_v/dev/vars.tf
@@ -4,51 +4,56 @@ variable "arima_vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "arima_public_subnet_1" {
-  description = "CIDR value for public subnet"
-  type        = string
-  default     = "10.0.1.0/24"
-
+variable "arima_public_subnet" {
+  description = "CIDR value for Public subnets"
+  type        = map(string)
+  default = {
+    "pub_1" = "10.0.1.0/24"
+    "pub_2" = "10.0.2.0/24"
+  }
 }
 
-variable "arima_public_subnet_2" {
-  description = "CIDR value for public subnet"
-  type        = string
-  default     = "10.0.2.0/24"
+variable "arima_private_subnet" {
+  description = "CIDR value for private subnet"
+  type        = map(string)
+  default = {
+    "pri_1" = "10.0.3.0/24"
+    "pri_2" = "10.0.4.0/24"
+  }
 }
 
-variable "arima_private_subnet_1" {
-  description = "CIDE value for private subnet"
-  type        = string
-  default     = "us-east-1c"
+variable "arima_public_azs" {
+  description = "AZ for public subnet"
+  type        = map(string)
+  default = {
+    "pub_1" = "us-east-1a"
+    "pub_2" = "us-east-2a"
+  }
 }
 
-variable "arima_private_subnet_2" {
-  description = "CIDE value for private subnet"
-  type        = string
-  default     = "us-east-1d"
+variable "arima_private_azs" {
+  description = "AZ for private subnet"
+  type        = map(string)
+  default = {
+    "pri_1" = "us-east-1c"
+    "pri_2" = "us-east-1d"
+  }
 }
 
-variable "arima_public_subnet_az_1" {
-  description = "Public subnet AZs"
-  type        = string
-  default     = "us-east-1a"
-}
 
-variable "arima_public_subnet_az_2" {
-  description = "Public subnet AZs"
-  type        = string
-  default     = "us-east-1b"
+variable "ec2_ami_type" {
+  description = "EC2 AMI values"
+  type        = map(string)
+  default = {
+    "pub" = "ami-01816d07b1128cd2d"
+    "pri" = "ami-04b4f1a9cf54c11d0"
+  }
 }
-
-variable "arima_private_subnet_az_1" {
-  description = "Private subnet AZs"
-  type        = string
-  default     = "us-east-1c"
-}
-
-variable "arima_private_subnet_az_2" {
-  description = "Private subnet AZs"
-  type        = string
-  default     = "us-east-1d"
+variable "ec2_instance_type" {
+  description = "EC2 instance types"
+  type        = map(string)
+  default = {
+    "pub" = "t3.medium"
+    "pri" = "t2.medium"
+  }
 }
